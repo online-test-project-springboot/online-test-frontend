fbde4892e62ff9a2e537e11a490de2da55822af8
Minh Nguyen <pmnguyen1032000@gmail.com>
2022-07-25 13:42:40 +0700

    use library moment to convert time

--- src/features/DoExam/components/ExpiredNotice.jsx
+++ src/features/DoExam/components/ExpiredNotice.jsx
@@ -7,7 +7,8 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import { Transition } from 'utils';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
+import moment from 'moment';
 
 ExpiredNotice.propTypes = {};
 
@@ -26,48 +27,67 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function ExpiredNotice(props) {
+function ExpiredNotice({ open, resultExam }) {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  // const resultExam = {
+  //   examPersonName: 'user',
+  //   topicName: 'phep cong lop 1',
+  //   examName: 'kiem tra 15 phut - lop 1 - hoc ki 1 - 2022',
+  //   time: 15,
+  //   startTime: '2022-07-24T10:38:07.043694',
+  //   endTime: '2022-07-24T11:52:53.494943',
+  //   numOfQuestions: 3,
+  //   numOfRightAnswer: 1,
+  // };
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
 
-  const handleClose = (event, reason) => {
-    if (reason !== 'backdropClick') {
-      setOpen(false);
-    }
-  };
+  // const handleClose = (event, reason) => {
+  //   if (reason !== 'backdropClick') {
+  //     setOpen(false);
+  //   }
+  // };
   return (
     <div className={classes.expiredNotice}>
       <span>HẾT GIỜ !</span>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+      {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Slide in alert dialog
-      </Button>
+      </Button> */}
       <Dialog
         disableEscapeKeyDown
         open={open}
         TransitionComponent={Transition}
         keepMounted
-        onClose={handleClose}
+        // onClose={handleClose}
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogTitle id="alert-dialog-slide-title">Thông tin bài thi</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
-          </DialogContentText>
+          <Typography>Bài thi: {resultExam.examName}</Typography>
+          <Typography>Chủ để: {resultExam.topicName}</Typography>
+          <Typography>Thí sinh: {resultExam.examPersonName}</Typography>
+          <Typography>Thời gian làm bài: {resultExam.time}</Typography>
+          <Typography>
+            Thời gian bắt đầu làm bài: {moment(resultExam.startTime).format('LTS')}
+          </Typography>
+          <Typography>
+            Thời gian kết thúc làm bài: {moment(resultExam.endTime).format('LTS')}
+          </Typography>
+          <Typography>Tổng số câu hỏi: {resultExam.numOfQuestions}</Typography>
+          <Typography>
+            Số câu đúng: {resultExam.numOfRightAnswer}/{resultExam.numOfQuestions}
+          </Typography>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          {/* <Button onClick={handleClose} color="primary">
             Disagree
           </Button>
           <Button onClick={handleClose} color="primary">
             Agree
-          </Button>
+          </Button> */}
         </DialogActions>
       </Dialog>
     </div>
